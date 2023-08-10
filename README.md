762d010cc806ccc0ed141510720dd3028faa3533
Miguel Angel Velazco Martinez <mvelazco@sparefoot.com>
2023-08-10 18:26:10 -0500

    Code addition

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # dapr_pubsub_poc
 Proof of concept of Dapr pub-sub feature with PHP 8 and Slim Framework
+
+# Requirements
+
+PHP 8 and composer installed as well as [dapr CLI](https://docs.dapr.io/getting-started/install-dapr-cli/) and Docker (dapr requirement).
+# Installation
+
+Download composer with [this instructions.](https://getcomposer.org/download/)
+
+Don't forget to do `chmod +x composer.phar` to add execution permisions to the binary.
+
+# Run the app
+
+```
+# inside the repo just do a `composer install`. This will install the PHP dependencies
+composer.phar install
+
+# Run php built-in PHP server
+php -S localhost:8000 index.php
+
+# (Optional) If you haven't made it already
+dapr init
+
+# On a separate session you can run the dapr sidecart
+dapr run --app-port 8000 --app-id php-subscriber --app-protocol http --dapr-http-port 3500 --resources-path dapr
+```
+
+# Endpoints available
+
+Use the postman collection to test it.
+
+# Useful commands
+
+```
+# to list the instances of `dapr run` in execution
+dapr list
+
+# To use the dapr CLI to publish and trigger the subscription endpoint
+dapr publish --publish-app-id php-subscriber --pubsub php-subscriber --topic topic1 --data '{"hello": "world"}'
+
+# To list the process running on port 8000 (Works on OS X, if not installed, install it with brew)
+lsof -i :8000
+```
---
+++ index.php
@@ -0,0 +1,111 @@
+<?php
+
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/vendor/autoload.php';
+
+/**
+ * Instantiate App
+ *
+ * In order for the factory to work you need to ensure you have installed
+ * a supported PSR-7 implementation of your choice e.g.: Slim PSR-7 and a supported
+ * ServerRequest creator (included with Slim PSR-7)
+ */
+$app = AppFactory::create();
+
+/**
+ * The routing middleware should be added earlier than the ErrorMiddleware
+ * Otherwise exceptions thrown from it will not be handled by the middleware
+ */
+$app->addRoutingMiddleware();
+
+/**
+ * Add Error Middleware
+ *
+ * @param bool                  $displayErrorDetails -> Should be set to false in production
+ * @param bool                  $logErrors -> Parameter is passed to the default ErrorHandler
+ * @param bool                  $logErrorDetails -> Display error details in error log
+ * @param LoggerInterface|null  $logger -> Optional PSR-3 Logger
+ *
+ * Note: This middleware should be added last. It will not handle any exceptions/errors
+ * for middleware added after it.
+ */
+$errorMiddleware = $app->addErrorMiddleware(true, true, true);
+
+$app->get('/', function ($req, $response, $args) {
+    $response = $response->withHeader('Content-Type', 'application/json');
+    $response->getBody()->write(
+        json_encode([
+            'msg' => 'hello'
+        ])
+    );
+
+    return $response->withStatus(200);
+});
+
+$app->options('/', function ($req, $response, $args) {
+    $response = $response->withHeader('Content-Type', 'application/json');
+    $response->getBody()->write(
+        json_encode([
+            'msg' => 'hello'
+        ])
+    );
+
+    return $response->withStatus(200);
+});
+
+$app->options('/dapr/subscribe', function ($request, $response, $args) {
+    $response = $response->withHeader('Content-Type', 'application/json');
+    $response->getBody()->write(
+        json_encode([
+            [
+                'pubsubname' => 'php-subscriber',
+                'topic' => 'topic1',
+                'route' => 'orders'
+                ]
+        ], JSON_PRETTY_PRINT)
+    );
+
+    return $response->withStatus(200);
+});
+
+$app->get('/dapr/subscribe', function ($request, $response, $args) {
+    $response = $response->withHeader('Content-Type', 'application/json');
+    $response->getBody()->write(
+        json_encode([
+            [
+                'pubsubname' => 'php-subscriber',
+                'topic' => 'topic1',
+                'route' => 'orders'
+                ]
+        ], JSON_PRETTY_PRINT)
+    );
+
+    return $response->withStatus(200);
+});
+
+
+$app->options('/orders', function ($request, $response, $args) {
+    $response = $response->withHeader('Content-Type', 'application/json');
+    $response->getBody()->write(
+        json_encode([
+           'message' => 'DAPR ENDPOINT CALLED'
+        ], JSON_PRETTY_PRINT)
+    );
+
+    return $response->withStatus(200);
+});
+
+$app->post('/orders', function ($request, $response, $args) {
+    $response = $response->withHeader('Content-Type', 'application/json');
+    $response->getBody()->write(
+        json_encode([
+           'message' => 'DAPR ENDPOINT CALLED'
+        ], JSON_PRETTY_PRINT)
+    );
+
+    return $response->withStatus(200);
+});
+
+
+$app->run();
